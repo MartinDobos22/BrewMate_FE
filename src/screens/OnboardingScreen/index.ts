f21b07109d56d15cd65423edce0b531e346b55e2
dobos <dobosmartin595@gmail.com>
2025-11-28 21:42:20 +0100

    clean up code

--- src/screens/OnboardingScreen/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Entry point exports for the personalization onboarding flow to simplify imports across navigation.
- */
-export { default } from './OnboardingScreen';
-export type { Props as OnboardingScreenProps } from './OnboardingScreen';
