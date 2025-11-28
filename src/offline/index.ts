f29533fb2f2d1e09f8671e0d79893779864c291b
dobos <dobosmartin595@gmail.com>
2025-11-28 21:50:13 +0100

    clean up code

--- src/offline/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Aggregated exports for offline capabilities, enabling simplified imports for sync and cache managers.
- */
-export { coffeeOfflineManager } from './CoffeeOfflineManager';
-export { offlineSync } from './OfflineSync';
